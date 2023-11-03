216aa484932090812aeec8c1dfc7d94b81f024c5
thabet-fix <thabet_jmal@yahoo.fr>
2023-11-03 14:51:29 +0100

    initializing the first style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import SSRProvider from "react-bootstrap/SSRProvider";
+import Layout from "../components/layout";
+import '@fontsource/pt-serif/400.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SSRProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
