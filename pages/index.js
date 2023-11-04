4ae304bdcb9214b0a1346fabad3b0e40fcb1f23a
thabet-fix <thabet_jmal@yahoo.fr>
2023-11-04 11:04:46 +0100

    Figma to html static home page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,113 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import Container from "react-bootstrap/Container";
+import Carousel from "react-bootstrap/Carousel";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Container>
-        <div className='row'>
-          <div className='col-sm-6'>
-            hello
+        <div className="row custom-gutter">
+          <div className="col-md-6">
+            <video width="100%" height="auto" controls className="rounded-box">
+              <source src="/vercel.mp4" type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
+            <div className="testimonies rounded-box">
+              <Carousel
+                data-bs-theme="dark"
+                indicators={false}
+                prevIcon={
+                  <span
+                    aria-hidden="true"
+                    className="carousel-control-prev-icon"
+                  />
+                }
+                nextIcon={
+                  <span
+                    aria-hidden="true"
+                    className="carousel-control-next-icon"
+                  />
+                }>
+                <Carousel.Item>
+                  <Image
+                    src="/testimonial.png"
+                    width={62}
+                    height={62}
+                    alt="change it"
+                  />
+                  <Carousel.Caption>
+                    <h3>Julie Ruberta</h3>
+                    <p>
+                      Nulla vitae elit libero, a pharetra augue mollis interdum.
+                      Nulla vitae elit libero, a pharetra augue mollis interdum
+                      Nulla vitae elit libero, commodo cursus magna, vel scelerisque nisl
+                      consectetur. Praesent commodo a pharetra augue mollis
+                      interdumNulla vitae elit libero, a pharetra augue mollis
+                      interdumNulla vitae elit libero, a pharetra augue mollis
+                      interdum
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Image
+                    src="/testimonial.png"
+                    width={62}
+                    height={62}
+                    alt="change it"
+                  />
+                  <Carousel.Caption>
+                    <h3>Julie Ruberta</h3>
+                    <p>
+                      Lorem ipsum dolor sit amet, consectetur adipiscing
+                      scelerisque nisl consectetur. Praesent commodo curs
+                      consectetur, vel scelerisque nisl consectetur. Praesent
+                      commodo cursus magna, vel scelerisque nisl consectetur.
+                      Praesent commodo curs consectetur, vel scelerisque nisl
+                      consectetur. Praesent commodo cursus magna, vel.Lorem
+                      ipsum dolor sit amet.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                  <Image
+                    src="/testimonial.png"
+                    width={62}
+                    height={62}
+                    alt="change it"
+                  />
+                  <Carousel.Caption>
+                    <h3>Julie Ruberta</h3>
+                    <p>
+                      Praesent commodo cursus magna, vel scelerisque nisl
+                      consectetur. Praesent commodo curs consectetur, vel
+                      scelerisque nisl consectetur. Praesent commodo cursus
+                      magna, vel scelerisque nisl consectetur. Praesent commodo
+                      cursus magna, vel scelerisque nisl consectetur scelerisque
+                      nisl consectetur. Praesent commodo curs consectetur.
+                    </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </div>
           </div>
-          <div className='col-sm-6'>
-            world
+          <div className="col-md-6">
+            <div className="upload-cv rounded-box">
+              <Image
+                src="/cv-icon.png"
+                alt="upload cv"
+                width={94}
+                height={94}
+              />
+              <h2>Upload CV</h2>
+              <p>( PDF or DOCX )</p>
+            </div>
+            <div className="create-cv rounded-box">
+              <h2>Create CV with AI</h2>
+            </div>
           </div>
         </div>
       </Container>
     </div>
-  )
+  );
 }
